81ec60e0d878a9d379762c72be8a9e5ed73f62c8
Pieter-Jan Van de Perre <pieter-janvandeperre@MacBook-Pro-van-Pieter-Jan.local>
2014-12-09 15:55:27 +0100

    Added ability to drop bombs
    Use SPACE to drop a standard bomb

---
+++ Assets/BombDropping.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombDropping : MonoBehaviour {
+
+	public Transform rb_standard_bomb;
+	public Transform rb_v2_bomb;
+	public Transform rb_mustard_bomb;
+
+	enum eBombTypes {
+		BOMB_TYPE_STANDARD,
+		BOMB_TYPE_V2,
+		BOMB_TYPE_MUSTARD
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) { dropBomb(eBombTypes.BOMB_TYPE_STANDARD); }
+	
+	}
+
+	void dropBomb(eBombTypes bombType)
+	{
+		switch (bombType) 
+		{
+			case eBombTypes.BOMB_TYPE_STANDARD:
+			{				
+				Rigidbody clone;
+				clone = Instantiate (rb_standard_bomb, transform.position, transform.rotation) as Rigidbody;
+				clone.velocity = transform.TransformDirection (Vector3.forward * 10);
+				break;
+			}
+			default:
+			{
+				break;
+				// DO SOMETHING SHEEIIITTTT
+			}
+		}
+	}
+}
+	
