6f93219ac550a7482b281ed4e7a2ab3a75e74961
Thomas D'haen <thomasdhaen@me.com>
2014-12-09 13:21:09 +0100

    added bombz!

---
+++ Assets/GameSetup.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var mainCam : Camera;
+
+var rightWall : BoxCollider;
+
+function Start () {
+
+rightWall.size = new Vector3(mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f, 1f);
+rightWall.center = new Vector3(0f, mainCam.ScreenToWorldPoint (new Vector3 (0f, Screen.height, 0f)).y + 0.5f, 0f);
+
+}
+
+function Update () {
+
+}
